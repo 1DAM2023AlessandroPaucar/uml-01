accd9c0bf25be89105e9e3572b9583a0892fb831
alumno_alessandro <alessandroj.pau@educa.jcyl.es>
2023-10-05 16:18:32 +0200

    Inicio del proyecto

---
+++ src/PlacaBase.java
@@ -0,0 +1,24 @@
+public class PlacaBase {
+
+    public String fabricante;
+
+    public String modelo;
+
+    public Double precio;
+
+    public String getFullname(){
+
+        return fabricante + "" + modelo + "" + precio;
+
+    }
+
+    public String getMarca;
+
+
+
+
+
+
+
+
+}
---
+++ src/Procesador.java
@@ -0,0 +1,29 @@
+public class Proces ador {
+
+            // Estados, atributos, propiedades
+            public String marca;
+            public String modelo;
+            public Double precio;
+
+            // Comportamiento o método (funciones)
+            // public + método accesible desde cualquier otra clase
+            // String: tipo devuelto por el método
+            public String getNombreCompleto(){
+                // Concatenar cadenas con +
+
+                return marca + " "  + modelo + " " + precio;
+
+            }
+            public String getMarca(){
+
+            }
+            public String getModelo(){
+
+
+            }
+            public Double getPrecio(){
+
+            }
+}
+
+
