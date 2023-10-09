5efdb0af67a9f43db71e91413f2c4d50c29d94f9
alumno_alessandro <alessandroj.pau@educa.jcyl.es>
2023-10-09 18:00:39 +0200

    Nuevo commit despues de clonar el proyecto

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,14 @@
 public class Main {
     public static void main(String[] args) {
 
+        Motherboard motherboard = new Motherboard();
+        motherboard.setId(1);
+        motherboard.setModel("Model-01");
+        motherboard.setManufacturer("ASUS");
+
+        System.out.println(motherboard.getId());
+        System.out.println(motherboard.getModel());
+        System.out.println(motherboard.getManufacturer());
     }
 }
 
---
+++ src/Motherboard.java
@@ -0,0 +1,33 @@
+public class Motherboard {
+
+        private Integer id;
+
+        private String model;
+
+        private String manufacturer;
+
+
+        public Integer getId(){
+            return id;
+        }
+
+        public String getModel(){
+            return model;
+        }
+        public String getManufacturer(){
+            return manufacturer;
+        }
+
+        public void setId(Integer id){
+            this.id = id;
+        }
+        public void setModel(String model) {
+            this.model = model;
+        }
+        public void setManufacturer( String manufacturer) {
+            this.manufacturer = manufacturer;
+        }
+
+    }
+
+
